a11a8f3ee1aa8f2749d53761cf819125fc40d12a
keple <keplerde@gmail.com>
2023-10-25 21:00:13 +0200

    search_key

--- jobby/job_app/filters.py
+++ jobby/job_app/filters.py
@@ -3,7 +3,13 @@ from .models import Job
 
 class JobsFilter(filters.FilterSet):
 
-    # Фильтрация по минимальной зарплате (поле 'salary') - значение должно быть больше или равно заданной величины
+    # Фильтрация по ключевому слову (поле 'title') - ищем вхождения без учета регистра
+    keyword = filters.CharFilter(field_name='title', lookup_expr='icontains')
+
+    # Фильтрация по местоположению (поле 'address') - ищем вхождения без учета регистра
+    location = filters.CharFilter(field_name='address', lookup_expr='icontains')
+
+    # Фильтрация по минимальной зарплате (поле 'salary') - значение должно быть больше или равно заданной величине
     min_salary = filters.NumberFilter(field_name="salary" or 0, lookup_expr='gte')
 
     # Фильтрация по максимальной зарплате (поле 'salary') - значение должно быть меньше или равно заданной величине
@@ -11,4 +17,4 @@ class JobsFilter(filters.FilterSet):
 
     class Meta:
         model = Job
-        fields = ('education', 'jobType', 'experience', 'min_salary', 'max_salary')
+        fields = ('keyword', 'location', 'education', 'jobType', 'experience', 'min_salary', 'max_salary')
