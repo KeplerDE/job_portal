6efbc1140b7eef60ad41c04f1b14e207bdd0b54e
keple <keplerde@gmail.com>
2023-11-13 16:42:33 +0100

    adding UserUpdateProfile

---
+++ frontend/pages/me/index.js
@@ -0,0 +1,12 @@
+import Layout from "../../components/layout/Layout";
+import UpdateProfile from "../../components/user/UpdateProfile";
+
+
+export default function UpdateProfilePage() {
+  return (
+    <Layout title="Update User Profile">
+      <UpdateProfile />
+    </Layout>
+  );
+}
+
