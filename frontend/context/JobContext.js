eaf95d2b0f89f4d067953654479c7cd31f1c467f
keple <keplerde@gmail.com>
2023-11-16 18:42:37 +0100

    adding Update, Delete

--- frontend/context/JobContext.js
+++ frontend/context/JobContext.js
@@ -7,7 +7,10 @@ export const JobProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [created, setCreated] = useState(null);
-
+  const [updated, setUpdated] = useState(null);
+  const [deleted, setDeleted] = useState(null);
+  const [applied, setApplied] = useState(false);
+  const [stats, setStats] = useState(false);
 
   // Create a new job
   const newJob = async (data, access_token) => {
@@ -38,19 +41,128 @@ export const JobProvider = ({ children }) => {
   };
 
   // Update job
+  const updateJob = async (id, data, access_token) => {
+    try {
+      setLoading(true);
 
+      const res = await axios.put(
+        `${process.env.API_URL}/api/jobs/${id}/update/`,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      if (res.data) {
+        setLoading(false);
+        setUpdated(true);
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(
+        error.response &&
+          (error.response.data.detail || error.response.data.error)
+      );
+    }
+  };
 
   // Apply to Job
+  const applyToJob = async (id, access_token) => {
+    try {
+      setLoading(true);
 
+      const res = await axios.post(
+        `${process.env.API_URL}/api/jobs/${id}/apply/`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      if (res.data.applied === true) {
+        setLoading(false);
+        setApplied(true);
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(
+        error.response &&
+          (error.response.data.detail || error.response.data.error)
+      );
+    }
+  };
 
   // Check job applied
+  const checkJobApplied = async (id, access_token) => {
+    try {
+      setLoading(true);
 
+      const res = await axios.get(
+        `${process.env.API_URL}/api/jobs/${id}/check/`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      setLoading(false);
+      setApplied(res.data);
+    } catch (error) {
+      setLoading(false);
+      setError(
+        error.response &&
+          (error.response.data.detail || error.response.data.error)
+      );
+    }
+  };
 
   // Get topic stats
+  const getTopicStats = async (topic) => {
+    try {
+      setLoading(true);
 
+      const res = await axios.get(`${process.env.API_URL}/api/stats/${topic}/`);
+
+      setLoading(false);
+      setStats(res.data);
+    } catch (error) {
+      setLoading(false);
+      setError(
+        error.response &&
+          (error.response.data.detail || error.response.data.error)
+      );
+    }
+  };
 
   // Delete job
+  const deleteJob = async (id, access_token) => {
+    try {
+      setLoading(true);
 
+      const res = await axios.delete(
+        `${process.env.API_URL}/api/jobs/${id}/delete/`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      setLoading(false);
+      setDeleted(true);
+    } catch (error) {
+      setLoading(false);
+      setError(
+        error.response &&
+          (error.response.data.detail || error.response.data.error)
+      );
+    }
+  };
 
   // Clear Errors
   const clearErrors = () => {
@@ -63,8 +175,19 @@ export const JobProvider = ({ children }) => {
         loading,
         error,
         created,
+        updated,
+        deleted,
+        applied,
+        stats,
         newJob,
+        updateJob,
+        deleteJob,
+        getTopicStats,
+        applyToJob,
+        setUpdated,
+        checkJobApplied,
         setCreated,
+        setDeleted,
         clearErrors,
       }}
     >
